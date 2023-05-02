27506876a281f8977b6f337313d32e42b2eb84c0
ReeZey <johannes@seved.net>
2023-05-02 17:02:15 +0200

    supports multiple formats, and update readme

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
 # riff extractor
 
-useful for extracting WAVE files from a single file
-
+useful for extracting WAVE files from a single file  
 made in Rust!
+
+## How do i use this?
+Simply download the executable from releases and drag & drop files that you want unpacked ontop of the executable
+
+*Note: Currently there is no way to choose where it puts the files  
+so it creates a folder called `riff_output` in the working directory
+
+## Currently supporting 
+| Format  | Extension |
+|:-------:|:---------:|
+| WEBP    | .webp     |
+| WAVE    | .wav      |
+| AVI     | .avi      |
+
+If it encounters a unknown format, the file extension is the RIFF Format
+
+Feel free to add more with a pull request here:
+```
+[main.rs]
+fn get_type(array: &[u8]) -> &str {
+    return match array {
+        b"WAVE" => "wav",
+        b"WEBP" => "webp",
+        b"AVI " => "avi",
+        b"NEW " => "new", <--- like this
+        _ => &std::str::from_utf8(array).unwrap(),
+    }
+}
+```
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() -> std::io::Result<()> {
     }
 
     let args: Vec<String> = env::args().collect();
-    if args.len() < 1 {
+    if args.len() < 2 {
         println!("bad arguments");
         return Ok(());
     }
@@ -21,19 +21,21 @@ fn main() -> std::io::Result<()> {
         if &buffer == b"RIFF" {
             file.seek(SeekFrom::Start(cursor)).unwrap();
             let parent = Path::new(&filename).parent().expect("could not find parent folder");
-            let filename = format!("{}/{}-{cursor}.wav", parent.join(foldername).to_string_lossy(), filename.file_name().unwrap().to_string_lossy());
 
             println!("found new riff at offset {}", cursor);
 
-            let mut new_file = File::create(&filename).expect("could not save new file");
             file.read_exact(&mut buffer)?; //RIFF
             file.read_exact(&mut buffer)?; //ChunkData Length
+            let total_size = i32::from_le_bytes(buffer) - 4;
+            file.read_exact(&mut buffer)?; //Format
+            let file_type = get_type(&buffer);
 
-            let total_size = i32::from_le_bytes(buffer);
+            let new_filename = format!("{}/{}-{cursor}.{file_type}", parent.join(foldername).to_string_lossy(), filename.file_name().unwrap().to_string_lossy());
+            let mut new_file = File::create(&new_filename).expect("could not save new file");
 
             new_file.write(b"RIFF").expect("could not write to file");
             new_file.write(&total_size.to_le_bytes()).expect("could not write to file");
-
+            new_file.write_all(&buffer).expect("could not write to file");
             
             let mut buffer = vec![0; total_size as usize];
             file.read_exact(&mut buffer).unwrap();
@@ -45,4 +47,13 @@ fn main() -> std::io::Result<()> {
     };
 
     Ok(())
+}
+
+fn get_type(array: &[u8]) -> &str {
+    return match array {
+        b"WAVE" => "wav",
+        b"WEBP" => "webp",
+        b"AVI " => "avi",
+        _ => &std::str::from_utf8(array).unwrap(),
+    }
 }
